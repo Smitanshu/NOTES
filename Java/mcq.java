cdf74ab9f4b092d047cedfa38e0ca6873b9b9c23
CraftSyntax <150527585+Smitanshu@users.noreply.github.com>
2024-07-03 11:48:24 +0530

    mod

---
+++ Java/mcq.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class mcq {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+
+    }
+}
+/*
+ * public class mcq {
+ * public static void main(){
+ * System.out.println("Welcome");
+ * }
+ * 
+ * }
+ * 
+ */
