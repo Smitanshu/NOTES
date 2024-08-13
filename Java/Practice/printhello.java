5142b55e4eae1f3c468515c922266b83a8125eb0
CraftSyntax <150527585+Smitanshu@users.noreply.github.com>
2024-08-13 19:45:52 +0530

    assignment problem of java

--- Java/Practice/printhello.java
+++ Java/Practice/printhello.java
@@ -1,10 +1,12 @@
 package Practice;
 
+
+
 public class printhello {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-
+        System.out.print("Hello");
     }
 }
