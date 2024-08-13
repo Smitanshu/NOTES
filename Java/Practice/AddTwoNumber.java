ed407654de830689fdd179773d3b80ce16dd25b6
CraftSyntax <150527585+Smitanshu@users.noreply.github.com>
2024-08-13 20:02:05 +0530

    assignment problem of java

---
+++ Java/Practice/AddTwoNumber.java
@@ -0,0 +1,17 @@
+package Practice;
+import  java.util.Scanner;
+public class AddTwoNumber {
+
+    public static void main(String[] args){
+       Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int num1=sc.nextInt();
+        System.out.println("Enter Second number");
+        int num2=sc.nextInt();
+        int ans=num1+num2;
+        System.out.println("Addition :"+ ( num1+num2));
+
+
+
+    }
+}
