efb3cdecb70cae3b42c1db0f14acca2e12af132d
CraftSyntax <150527585+Smitanshu@users.noreply.github.com>
2024-11-22 21:57:22 +0530

    Local Push

---
+++ Java/StringLiterals/DemoStringLiterals.java
@@ -0,0 +1,11 @@
+package StringLiterals;
+
+public class DemoStringLiterals {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+
+        //The String which located in the double quote is called as String .
+    }
+}
